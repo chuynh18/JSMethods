c692c0388f075c91a345b1dcbab8fed6f5fc139d
5820K <38088640+chuynh18@users.noreply.github.com>
2018-07-16 22:15:38 -0700

    lastIndexOf.js written

--- String methods/lastIndexOf.js
+++ String methods/lastIndexOf.js
@@ -4,3 +4,25 @@
 
 const string = "This is an example string.  It is very interesting.";
 
+const lastIndexOf = function(input, substring) {
+    for (let i = input.length - substring.length; i >= 0; i--) {
+        let match = true;
+        for (let j = 0; j < substring.length; j++) {
+            if (input[i+j] !== substring[j]) {
+                match = false;
+                break;
+            };
+        };
+        if (match) {
+            return i;
+        };
+    };
+    return -1;
+};
+
+// ================================================
+
+console.log(lastIndexOf(string, "is"));
+console.log(lastIndexOf(string, "ing"));
+console.log(lastIndexOf(string, "string"));
+console.log(lastIndexOf(string, "Nikon D500"));
