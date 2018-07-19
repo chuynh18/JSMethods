cb4646c1b64a6cee3f39e84a2d4a1dfdb3308fb8
5820K <38088640+chuynh18@users.noreply.github.com>
2018-07-19 11:52:13 -0700

    simplified split logic by removing unnecessary special case checks, commented the code (overkill on the comments???)

--- String methods/split.js
+++ String methods/split.js
@@ -23,65 +23,82 @@ the matching text.
 
 There is some extra conditional logic to handle special cases.
 
-First, in the event that the search term is an empty string, we want our array of indices to
-start out empty.  This is because an empty search term will match at every index, so we'll
-have an extraneous element in our array of indices (specifically, an extra 0).
+First, we check to see if the input is an empty string.  In the event that it is, we want to
+return an empty array.
 
-Next, we check to see if our array of indices is greater than 2.  This is because it being 2
-implies that no matches were found against our search term, in which case, we want to return
-the input string inside an array.
-
-Lastly, we check to see if the input is an empty string.  In the event that it is, we want to
-return an empty array. */
+In the event that the search term is an empty string, we want our array of indices to start
+out empty.  This is because an empty search term will match at every index, so we'll have an
+extraneous element in our array of indices (specifically, an extra 0). */
 
 const split = function(input, search) {
+
+    // =================== FIRST HALF ===================
+    // This part of the function searches through the input string for matches and builds an
+    // array storing the indices of all the match locations.  We'll use that array to build
+    // the substrings in the second half.
+
+    // if input is an empty string, return an empty array so we match JS's split behavior.
+    if (input === "") {
+        return [];
+    }
+
+    // indices holds the array we'll build to help us split the string
     let indices = [-search.length];
+    // output is the array we'll eventually return
     let output = [];
 
+    // if the search term is an empty string, we want to start with an empty array
+    // otherwise we'll have a duplicate entry (duplicate 0)
     if (search === "") {
         indices = [];
     };
 
+    // the outer loop handles looping through the entire input string
     for (let i = 0; i < input.length; i++) {
+        // start by assuming we have a match (I believe the logic is more succinct this way)
         let match = true;
 
+        // check input string against search string.
         for (let j = 0; j < search.length; j++) {
+            // the moment we have a mismatch...
             if (input[i+j] !== search[j]) {
+                // set match to false and break out of this loop
                 match = false;
                 break;
             };
         };
 
+        // if we ended the inner loop without a mismatch, match is still true
         if (match) {
+            // this is equivalent to pushing the value of i into our indices array
+            // perhaps a better name would have been "matches"?
             indices[indices.length] = i;
         };
     };
 
+    // the last element in the indices array is the length of the input string
+    // if we don't have this, we'll end up missing the last substring
     indices[indices.length] = input.length;
 
-    if (indices.length > 2) {
-        for (let k = 0; k < indices.length - 1; k++) {
-            let splitArrayElement = "";
+    // =================== SECOND HALF ===================
+    // now we loop through the indices array to build out the array of substrings
 
-            for (let l = indices[k] + search.length; l < indices[k+1]; l++) {
-                splitArrayElement += input[l];
-            };
+    for (let k = 0; k < indices.length - 1; k++) {
+        // temporary variable to hold the substring as we build it
+        let splitArrayElement = "";
 
-            if (splitArrayElement === search) {
-                output[k] = "";
-            }
-            else {
-                output[k] = splitArrayElement;
-            };
+        // We look at elements in the indices array two at a time, "sweeping" from the first
+        // to the next.  Our start index in this loop has search.length added to it.
+        // This is so that the search term does not appear in our array of substrings
+        for (let l = indices[k] + search.length; l < indices[k+1]; l++) {
+            splitArrayElement += input[l];
         };
-        return output;
-    }
-    else if (input === "") {
-        return [];
-    }
-    else {
-        return [input];
+
+        // Now we actually insert the substring into the array of substrings.
+            output[k] = splitArrayElement;
+
     };
+    return output;
 };
 
 // ================================================
